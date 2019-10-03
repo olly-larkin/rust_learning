f61d04483c0be7b65061a5c5b33cd7b88cde8eed
Olly Larkin <ollylemonlarkin@gmail.com>
2019-10-03 13:36:07 +0100

    finished bowling

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,18 +4,100 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+pub struct BowlingGame {
+    bowls: Vec<u16>,
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        BowlingGame { bowls: Vec::new() }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if self.game_complete() {
+            Err(Error::GameComplete)
+        } else if pins > 10 {
+            Err(Error::NotEnoughPinsLeft)
+        } else {
+            match self.bowls.len() {
+                0..=17 if self.bowls.len() % 2 == 0 => {
+                    self.bowls.push(pins);
+                    if pins == 10 {
+                        self.bowls.push(0);
+                    }
+                    Ok(())
+                },
+                0..=17 if self.bowls.len() % 2 != 0 => {
+                    if pins + self.bowls.last().unwrap() > 10 {
+                        Err(Error::NotEnoughPinsLeft)
+                    } else {
+                        self.bowls.push(pins);
+                        Ok(())
+                    }
+                },
+                18 => {
+                    self.bowls.push(pins);
+                    Ok(())
+                },
+                19 => if self.bowls[18] != 10 && self.bowls[18] + pins > 10 {
+                    Err(Error::NotEnoughPinsLeft)
+                } else {
+                    self.bowls.push(pins);
+                    Ok(())
+                },
+                20 => if self.bowls[18] == 10 && self.bowls[19] != 10 && self.bowls[19] + pins > 10 {
+                    Err(Error::NotEnoughPinsLeft)
+                } else {
+                    self.bowls.push(pins);
+                    Ok(())
+                },
+                _ => unreachable!(), 
+            }
+        }
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        if self.game_complete() {
+            let mut score = 0;
+            for (i, roll) in self.bowls.iter().enumerate().filter(|&(index, _)| index < 18) {
+                score += roll;
+                if *roll == 10 {            // when theres a strike
+                    let mut remaining = 2;
+                    for (_, val) in self.bowls.iter().enumerate().filter(|&(index, val)| index > i && *val != 0) {
+                        score += val;
+                        remaining -= 1;
+                        if remaining == 0 {
+                            break;
+                        }
+                    }
+                }
+                if i % 2 != 0 {     // check for spare
+                    if roll + self.bowls[i - 1] == 10 && *roll != 10 && self.bowls[i - 1] != 10 {   // spare found
+                        if let Some(next_val) = self.bowls
+                            .iter()
+                            .enumerate()
+                            .filter(|&(index, val)| index > i && *val != 0)
+                            .map(|(_, val)| *val)
+                            .next() {
+                                score += next_val;    
+                        }
+                    }
+                }
+            }
+            for (_, roll) in self.bowls.iter().enumerate().filter(|&(index, _)| index >= 18) {
+                score += roll;
+            }
+            Some(score)
+        } else {
+            None
+        }
+    }
+
+    pub fn game_complete(&self) -> bool {
+        match self.bowls.len() {
+            0..=19 => false,
+            20 => self.bowls[18] + self.bowls[19] < 10,
+            _ => true,
+        }
     }
 }
